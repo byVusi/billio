20a761f15f5a9c16407190c39cc78c9252ed4e0d
byVusi <mathonsi@duck.com>
2026-01-01 18:34:53 +0200

    new event handlers

--- assets/js/eventHandlers.js
+++ assets/js/eventHandlers.js
@@ -1,6 +1,51 @@
 function settingsButtonClickHandler(e) {
-	const clickedButton = e.target.closest("settings-button");
-	if (!clickedButton) return;
+	const clickedItem = e.target.closest("#settings-button");
+	if (!clickedItem) return;
 
 	console.log("Settings button clicked");
 }
+
+function dataValueClickHandler(e, container) {
+	const clickedItem = e.target.closest(".value");
+	if (!clickedItem) return;
+
+	// Remove .active from all data values
+	container.querySelectorAll(".value")?.forEach((element) => {
+		element.classList.remove("active");
+	});
+
+	clickedItem.classList.add("active");
+
+	console.log(clickedItem);
+}
+
+function counterClickHandler(e) {
+	const clickedItem = e.target.closest(".counter-control");
+	if (!clickedItem) return;
+
+	if (clickedItem?.id === "decrease") console.log("Decrease button clicked");
+
+	if (clickedItem?.id === "increase") console.log("Increase button clicked");
+}
+
+function numpadKeyClickHandler(e) {
+	const clickedItem = e.target.closest(".numpad-key");
+	if (!clickedItem) return;
+
+	const number = clickedItem.textContent;
+
+	if (number.length > 1) return; // return when backspace button is clicked
+
+	console.log(number);
+}
+
+export const EVENT_HANDLERS = {
+	CLICK: {
+		BUTTONS: {
+			SETTINGS: settingsButtonClickHandler,
+			COUNTER: counterClickHandler,
+			NUMPAD_KEYS: numpadKeyClickHandler,
+		},
+		DATA: dataValueClickHandler,
+	},
+};
