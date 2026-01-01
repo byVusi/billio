337dc215fa8d23ee421d1672f41fb95caa00d748
byVusi <mathonsi@duck.com>
2026-01-01 18:35:05 +0200

    add event handlers

---
+++ main.js
@@ -0,0 +1,24 @@
+import { EVENT_HANDLERS } from "./assets/js/eventHandlers.js";
+
+const BUTTONS = EVENT_HANDLERS.CLICK.BUTTONS;
+const NAV = document.querySelector("nav");
+const DATA_CONTAINER = document.getElementById("data-container");
+const SPLIT_CONTAINER = document.getElementById("split-container");
+const NUMPAD = document.getElementById("numpad");
+
+// Add initial event listeners to clickable items
+NAV.addEventListener("click", (e) => {
+	BUTTONS.SETTINGS(e);
+});
+
+SPLIT_CONTAINER.addEventListener("click", (e) => {
+	BUTTONS.COUNTER(e);
+});
+
+NUMPAD.addEventListener("click", (e) => {
+	BUTTONS.NUMPAD_KEYS(e);
+});
+
+DATA_CONTAINER.addEventListener("click", (e) => {
+	EVENT_HANDLERS.CLICK.DATA(e, DATA_CONTAINER);
+});
