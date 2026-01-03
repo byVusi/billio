3e19fe6f673b5d4a400e42b0a5cae637c1e5a787
byVusi <mathonsi@duck.com>
2026-01-03 16:52:11 +0200

    uploaded app.js

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+import { EVENT_HANDLERS } from "./modules/eventHandlers.js";
+import { setTheme } from "./theme.js";
+
+async function init() {
+	const BUTTONS = EVENT_HANDLERS.CLICK.BUTTONS;
+	const NAV = document.querySelector("nav");
+	const DATA_CONTAINER = document.getElementById("data-container");
+	const SPLIT_CONTAINER = document.getElementById("split-container");
+	const NUMPAD = document.getElementById("numpad");
+
+	// Initialise app theme
+	await setTheme();
+
+	// Add initial event listeners to clickable items
+	NAV.addEventListener("click", async (e) => {
+		const [theme, theme_palette] = await setTheme();
+		BUTTONS.SETTINGS(e, theme, theme_palette);
+	});
+
+	SPLIT_CONTAINER.addEventListener("click", (e) => {
+		BUTTONS.COUNTER(e);
+	});
+
+	NUMPAD.addEventListener("click", (e) => {
+		BUTTONS.NUMPAD_KEYS(e);
+	});
+
+	DATA_CONTAINER.addEventListener("click", (e) => {
+		EVENT_HANDLERS.CLICK.DATA(e, DATA_CONTAINER);
+	});
+}
+
+export const APP = {
+	INIT: init,
+};
