5e941ab20c539be0a0f3f7d69b68c8829175b002
byVusi <mathonsi@duck.com>
2026-01-03 16:51:58 +0200

    refactored code

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-import { EVENT_HANDLERS } from "./assets/js/modules/eventHandlers.js";
-import { setTheme } from "./assets/js/theme.js";
+import { APP } from "./assets/js/app.js";
 
-const BUTTONS = EVENT_HANDLERS.CLICK.BUTTONS;
-const NAV = document.querySelector("nav");
-const DATA_CONTAINER = document.getElementById("data-container");
-const SPLIT_CONTAINER = document.getElementById("split-container");
-const NUMPAD = document.getElementById("numpad");
-
-// Initialise app theme
-await setTheme();
-
-// Add initial event listeners to clickable items
-NAV.addEventListener("click", async (e) => {
-	const [theme, theme_palette] = await setTheme();
-	BUTTONS.SETTINGS(e, theme, theme_palette);
-});
-
-SPLIT_CONTAINER.addEventListener("click", (e) => {
-	BUTTONS.COUNTER(e);
-});
-
-NUMPAD.addEventListener("click", (e) => {
-	BUTTONS.NUMPAD_KEYS(e);
-});
-
-DATA_CONTAINER.addEventListener("click", (e) => {
-	EVENT_HANDLERS.CLICK.DATA(e, DATA_CONTAINER);
-});
+APP.INIT();
