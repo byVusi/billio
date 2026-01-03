a17397332c2fc8627808dd283d98ef51e8cd9251
byVusi <mathonsi@duck.com>
2026-01-03 16:45:04 +0200

    initialise app theme and edited event listener on nav

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { EVENT_HANDLERS } from "./assets/js/modules/eventHandlers.js";
+import { setTheme } from "./assets/js/theme.js";
 
 const BUTTONS = EVENT_HANDLERS.CLICK.BUTTONS;
 const NAV = document.querySelector("nav");
@@ -6,9 +7,13 @@ const DATA_CONTAINER = document.getElementById("data-container");
 const SPLIT_CONTAINER = document.getElementById("split-container");
 const NUMPAD = document.getElementById("numpad");
 
+// Initialise app theme
+await setTheme();
+
 // Add initial event listeners to clickable items
-NAV.addEventListener("click", (e) => {
-	BUTTONS.SETTINGS(e);
+NAV.addEventListener("click", async (e) => {
+	const [theme, theme_palette] = await setTheme();
+	BUTTONS.SETTINGS(e, theme, theme_palette);
 });
 
 SPLIT_CONTAINER.addEventListener("click", (e) => {
