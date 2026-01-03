71070309d5bfd4125cf8e2928435f8d144213431
byVusi <mathonsi@duck.com>
2026-01-03 16:45:27 +0200

    edited functions to set theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,26 @@
-const html = document.querySelector("html");
-const theme = html.dataset.theme;
-const palette = html.dataset.themePalette;
+import { STORAGE } from "./modules/storage.js";
 
-const storedTheme = STORAGE.FETCH() || "light";
-const storedPalette = STORAGE.FETCH() || "aether";
+const html = document.querySelector("html");
+
+export async function setTheme() {
+	const data = await STORAGE.FETCH();
+
+	html.dataset.theme = data.theme === "auto" ? isAuto() : data.theme;
+	html.dataset.themePalette = data.theme_palette;
+
+	const theme = data.theme;
+	const theme_palette = data.theme_palette;
+
+	return [theme, theme_palette];
+}
+
+function isAuto() {
+	const now = new Date();
+	const hours = now.getHours();
+
+	if (hours >= 6 && hours <= 17) {
+		return "light";
+	} else {
+		return "dark";
+	}
+}
