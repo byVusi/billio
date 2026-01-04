b5be029e67f95936eededa3dc60637239adbe6a1
byVusi <mathonsi@duck.com>
2026-01-04 20:21:55 +0200

    configure sw

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const REPO_NAME = "billio";
+const BASE_PATH = `/${REPO_NAME}`;
+
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+		.register(`${BASE_PATH}/sw.js`)
+		.then(() => console.log("✅ Service Worker registered"))
+		.catch((err) => console.error("❌ Service Worker failed:", err));
+}
+
 import { APP } from "./assets/js/app.js";
 
 APP.INIT();
